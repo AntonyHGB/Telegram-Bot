7f86781b251e8725659c4fc84a217f01483e39e8
AntonyHGB <antony0707jogos@gmail.com>
2021-04-29 20:32:24 -0300

    add_features_fixed

--- bot_tele_parrot.py
+++ bot_tele_parrot.py
@@ -20,6 +20,7 @@ def commands(update, context):
     update.message.reply_text('/commands you already understand')
     update.message.reply_text('/end will say bye bye to you')
 
+
 def end(update, context):
     update.message.reply_text('Bye Bye!')
 
@@ -38,6 +39,8 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("commands", commands))
+    dp.add_handler(CommandHandler("end", end))
 
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
